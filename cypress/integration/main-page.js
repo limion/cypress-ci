f6af80b5e6cb14ace31592fdb0d9cb2163fbc5bf
Vladyslav Holovko <vlad.holovko@gmail.com>
2020-02-24 17:52:45 +0200

    Create simple react app and test it

---
+++ cypress/integration/main-page.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context('Test Simple React App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  describe('When user opens the main page', () => {
+    it('Learn React link should be visible', () => {
+      cy.contains('Learn React')
+    })
+  })
+})
